30ed167e7bac16278df0452e21eaa73f6ff17e8a
longtime2024 <slametriyadi240202@gmail.com>
2024-07-04 19:37:40 +0700

    first commit

---
+++ logout.php
@@ -0,0 +1,26 @@
+<?php
+  require "php/db_connection.php";
+
+  if($con) {
+    $query = "UPDATE admin_credentials SET IS_LOGGED_IN = 'false'";
+    $result = mysqli_query($con, $query);
+  }
+  ?>
+
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title>Logout</title>
+      <script src="js/restrict.js"></script>
+    </head>
+    <body>
+      <script type="text/javascript">
+      window.location.href = "http://localhost/Pharmacy-Management/index.html";
+      </script>
+    </body>
+    <script>
+      
+    </script>
+  </html>
+
